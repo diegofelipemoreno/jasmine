4109efbc178059950a392597a87e248559acfe7f
diegofelipemoreno <diegofelipemoreno@gmail.com>
2020-01-13 00:42:16 -0500

    Jasmine Spies

--- investment/spec/NewInvestmentView.js
+++ investment/spec/NewInvestmentView.js
@@ -2,8 +2,9 @@ class NewInvestmentView {
     constructor(data) {
         this.element = document.querySelector(data.selector);
         this.stockInput = this.element.querySelector('.new-investment-stock-symbol');
-        this.submitInvestmentButton = this.element.querySelector('.submit-investment');
+        this.submitCta = this.element.querySelector('.submit-investment');
         this.investmentList = [];
+        this.name = data.name || '';
     }
 
     init = () => {
@@ -11,10 +12,41 @@ class NewInvestmentView {
     }
 
     events = () => {
-        this.submitInvestmentButton.addEventListener('click', (event)=> {
+        this.submitCta.addEventListener('click', (event)=> {
             event.preventDefault();
 
-            this.investmentList.push('investment');
+            this.createInvestment();
         });
+    }
+
+    onCreate = () => {
+        return this;
+    }
+
+    createInvestment = () => {
+        this.investmentList.push('investment');
+
+        this.onCreate();
+    }
+
+    create = (name) => {
+        this.name = name;
+
+        return this;
+    }
+
+    click = () => {
+        this.stockInput.click();
+
+        return 'clicked';
+    }
+
+    // Created to spy the name first and after could a get it.
+    setNameInvestment = (name) => {
+        this.name = name;
+    }
+
+    getInvestmentName = () => {
+        return `The name of the Investment is ${this.name}`;
     }
 }
--- investment/spec/NewInvestmentViewSpec.js
+++ investment/spec/NewInvestmentViewSpec.js
@@ -2,7 +2,7 @@ describe('NewInvestmentView', () => {
     const data = { selector: '#html-fixtures'};
     let newInvestmentView = {};
 
-    beforeAll(function () {
+    beforeAll(() => {
         newInvestmentView = new NewInvestmentView(data);
         newInvestmentView.init();
     });
@@ -23,11 +23,45 @@ describe('NewInvestmentView', () => {
     });
     
     it('should allow submit button on the DOM', () => {
-        expect(newInvestmentView.submitInvestmentButton).toBeInDOM();
+        expect(newInvestmentView.submitCta).toBeInDOM();
     });
 
     it('should add investment on click submit button', () => {
-        newInvestmentView.submitInvestmentButton.click();
+        newInvestmentView.submitCta.click();
         expect(newInvestmentView.investmentList.length).toBeGreaterThan(0);
+    });
+
+    describe ('With its inputs correctly filled', () => {
+
+        describe('and when an investment is created', () => {
+
+            beforeEach(() => {
+                spyOn(newInvestmentView, 'onCreate');
+                spyOn(newInvestmentView, 'create');
+                newInvestmentView.onCreate();
+                newInvestmentView.create('create-by-spy-purposes');
+            });
+
+            it ('should invoke the "onCreate" callback with the created investment', () => {
+                expect(newInvestmentView.onCreate).toHaveBeenCalled()
+                expect(newInvestmentView.create).toHaveBeenCalledWith('create-by-spy-purposes');
+            });
+        })
+    });
+
+    describe('with its inputs correctly filled', () => {
+        describe('and when the form is submitted', () => {
+            var callbackSpy;
+            var oso;
+
+            beforeEach(() => {
+                spyOn(newInvestmentView, 'createInvestment');
+                newInvestmentView.submitCta.click();
+            });
+
+            it('should create an investment', () => {
+                expect(newInvestmentView.createInvestment).toHaveBeenCalled();
+            });
+        });
     });
 });
